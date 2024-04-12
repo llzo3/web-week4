d8ca19436e34f280b9f5deff2ddad6ac83d62317
llzo3 <ryeod03@gmail.com>
2024-04-12 16:30:49 +0900

    4.12

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return { "message": "Hello World!"}
+    return { "Hello": "World!"}
 
-@app.get("/home")
-def home():
-    return { "message": "Home!" }
+@app.get("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
