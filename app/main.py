1c7ea92ef5f0dbb6ea4a4f175aed10cad3833d78
llzo3 <ryeod03@gmail.com>
2024-04-12 16:44:22 +0900

    4.12-2

--- app/main.py
+++ app/main.py
@@ -8,5 +8,5 @@ def root():
     return { "Hello": "World!"}
 
 @app.get("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+def update_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
