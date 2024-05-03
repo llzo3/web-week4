15e0894c5afd40bf0b54b642cc4796b536dc91c8
llzo3 <ryeod03@gmail.com>
2024-05-03 17:18:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -76,11 +76,7 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=4NJZM89F45s975GPowEIq1jYK2URj14t1p3ZBY92&returnType=json"
-
-    contents = requests.get(URL).text
-
-    return { "message": contents }
+    return { "message": "contents" }
 
 @app.get("/home")
 def home():
